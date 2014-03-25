c619b872057d8f00a5d959278645457eec9dd6d3
Emilien Baudet <emilien.baudet@gmail.com>
2014-03-25 21:34:09 +0100

    norme

--- srcs/normal.c
+++ srcs/normal.c
@@ -6,7 +6,7 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/24 20:07:22 by ebaudet           #+#    #+#             */
-/*   Updated: 2014/03/25 17:28:23 by ebaudet          ###   ########.fr       */
+/*   Updated: 2014/03/25 21:33:48 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,8 @@ void	vect_normal(t_vector *normal, t_vector *impact, void *object)
 	}
 	if (type_object(object) == 'c')
 	{
-		vector_set(normal, impact->x - ((t_cylind *)object)->pos->x 
-			, 0 , 
-			impact->z - ((t_cylind *)object)->pos->z);
+		vector_set(normal, impact->x - ((t_cylind *)object)->pos->x
+			, 0, impact->z - ((t_cylind *)object)->pos->z);
 	}
 	vector_normalize(normal);
 }
